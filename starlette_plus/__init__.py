a04bc0892b94aabb1c38daba8e9c78b34c5dffa9
MystyPy <evieepy@gmail.com>
2024-04-13 00:02:15 +1000

    Fix version.

--- starlette_plus/__init__.py
+++ starlette_plus/__init__.py
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = "1.0.0b"
+__version__ = "1.0.0"
 
 from starlette.requests import Request as Request
 from starlette.responses import (
